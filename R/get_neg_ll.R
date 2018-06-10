5efa744acd1a9f3a24d4860c4449d6d5a1ee94c5
liusi2019 <39580691+liusi2019@users.noreply.github.com>
2018-06-09 23:40:29 -0700

    Delete get_neg_ll.R

--- R/get_neg_ll.R
+++
@@ -1,9 +0,0 @@
-get_neg_ll <-
-function(a, w, hist, inf_replace=NA) {
-  x <- a %*% w
-  pdfs <- matrix(0, nrow=nrow(x), ncol=1)
-  pdfs[,1] <- pdf_hist(x, hist)
-  pdfs[,1] <- log(pdfs)
-  if (!is.na(inf_replace)) pdfs[,1] <- apply(pdfs, 1:2, max, inf_replace)
-  return(-pdfs) # neg. log-lik of pdf
-}
